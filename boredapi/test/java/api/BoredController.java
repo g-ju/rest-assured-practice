8c105490d022c9bc757860d91cf23cac1bd6f399
garth <52281882+g-ju@users.noreply.github.com>
2023-10-17 12:27:17 +0100

    Fix alignment

--- boredapi/test/java/api/BoredController.java
+++ boredapi/test/java/api/BoredController.java
@@ -21,79 +21,79 @@ public class BoredController
     public Activity getRandomActivity()
     {
         return given()
-                .spec(requestSpecification)
-                .get()
+                    .spec(requestSpecification)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     public Activity getActivityByKey(int key)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("key", key)
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("key", key)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     public Activity getActivityByType(Type type)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("type", type.toString())
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("type", type.toString())
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     // Note price is a factor in the range [0, 1]
     public Activity getActivityByPrice(double price)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("price", price)
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("price", price)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     public Activity getActivityWithinPriceRange(double minPrice, double maxPrice)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("minprice", minPrice)
-                .queryParam("maxprice", maxPrice)
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("minprice", minPrice)
+                    .queryParam("maxprice", maxPrice)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     public Activity getActivityWithAccessibility(double accessibility)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("accessibility", accessibility)
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("accessibility", accessibility)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 
     public Activity getActivityWithinAccessibilityRange(double minAccessibility, double maxAccessibility)
     {
         return given()
-                .spec(requestSpecification)
-                .queryParam("minaccessibility", minAccessibility)
-                .queryParam("maxaccessibility", maxAccessibility)
-                .get()
+                    .spec(requestSpecification)
+                    .queryParam("minaccessibility", minAccessibility)
+                    .queryParam("maxaccessibility", maxAccessibility)
+                    .get()
                .then()
-                .extract()
-                .as(Activity.class);
+                    .extract()
+                    .as(Activity.class);
     }
 }
